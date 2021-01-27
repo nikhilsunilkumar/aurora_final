c2bb032d1094d30ac7b3243a102d67608f24328e
DaniyaAnnammaAbraham <77786522+DaniyaAnnammaAbraham@users.noreply.github.com>
2021-01-27 15:37:19 +0530

    Update consolemanager.h

--- consolemanager.h
+++ consolemanager.h
@@ -55,20 +55,7 @@ typedef enum _parameter_id
  ***********************************/
 
 void uart_init(void);
-/**
- * @brief Enables the uart interrupt
- *
- * Sets the priority of the uart interrupt,
- *  and enables the reciever interrupts
- *
- * @param 	none.
- * @return 	none
- *
- * @note
- *
- * Revision History:
- * 	- 120121 DA: 	Creation Date
- */
+
 void enable_uart_isr();
 
 /*!
@@ -94,168 +81,25 @@ static inline void uart_msbf(UART_Type *base, bool enable)
     }
 }
 
-/**
- * @brief Receive the data from queue and return the data
- *
- * Stores the data from the queue, received from isr
- * function will be in the blocked state till the
- * data is recieved.
- * Displays the same data and returns the same.
- *
- * @param 	none.
- * @return 	data
- *
- * @note
- *
- * Revision History:
- * 	- 120121 DA: 	Creation Date
- */
 
 int get_keyinput();
-/**
- * @brief Receive the data from queue and return the data
- *
- * Stores the data from the queue, received from isr
- * function will be in the non blocked state and will
- * return immediately when no data in queue.
- * Displays the same data and returns the same.
- *
- * @param 	none.
- * @return 	data that is received from the uart
- *
- * @note
- *
- * Revision History:
- * 	- 200121 DA: 	Creation Date
- */
+
 int get_keyinput_nonblocking();
-/**
- * @brief Disables the uart receiver
- *
- * This function enables the UART receiver and
- * all the data from uart receiver is ignored.
- *
- * @param 	none.
- * @return 	none
- *
- * @note
- *
- * Revision History:
- * 	- 190121 DA: 	Creation Date
- */
+
 void uart_slave_enable();
-/**
- * @brief Creates new queue
- *
- * Create new Queue for to receive
- * the key from the  uart isr.
- *
- * @param 	none.
- * @return 	bool	success/failed
- *
- * @note
- *
- * Revision History:
- * 	- 120121 DA: 	Creation Date
- */
+
 int create_uartqueue();
 
-/**
- * @brief Displays a message
- *
- * This function displays the message
-
- *
- * @param 	const uint8_t *data	: message to be displayed
- * @return 	none
- *
- * @note
- *
- * Revision History:
- * 	- 130121 DA: 	Creation Date
- */
 void print_message(char *);
 
-/**
- * @brief Displays a message
- *
- * This function displays the message/data
-
- *
- * @param 	const uint8_t *data	: message to be displayed
- * 			size of the data displayed
- * @return 	none
- *
- * @note
- *
- * Revision History:
- * 	- 130121 DA: 	Creation Date
- */
 void print_data(uint8_t * data,uint8_t);
-/**
- * @brief validate the buffer recieved
- *
- * Function validates the buffer
- *
- * @param 	    id representing the which buffer
- * 				buffer to be validated
- * @return 	bool	success/failed
- *
- * @note
- *
- * Revision History:
- * 	- 120121 DA: 	Creation Date
- */
+
 int validate_buffer(uint8_t , uint8_t*);
 
-/**
- * @brief validate the byte recieved
- *
- * Function validates the byte based on the
- * recieved id and index.
- *
- * @param 	    id representing the which buffer
- * 				buffer to be validated
- * @return 	bool	success/failed
- *
- * @note
- *
- * Revision History:
- * 	- 120121 DA: 	Creation Date
- */
 int validate_byte(uint8_t, uint8_t , uint8_t);
 
-/**
- * @brief validate the byte received
- * during the execution.
- *
- * Function validates the byte based on the
- * recieved id and index.
- *
- * @param 	key/data to be validated
- * @return 	bool	success/failed
- *
- * @note
- *
- * Revision History:
- * 	- 140121 DA: 	Creation Date
- */
 int validate_executionkey(uint8_t);
-/**
- * @brief compares the start color and end color
- *
- * Function checks whether the start color is
- * smaller than the end color.
- *
- * @param 	Start color
- * 			End color
- * @return 	bool	success/failed
- *
- * @note
- *
- * Revision History:
- * 	- 140121 DA: 	Creation Date
- */
+
 int compare(uint8_t*, uint8_t*);
 
 
